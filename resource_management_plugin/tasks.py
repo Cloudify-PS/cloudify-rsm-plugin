910a9ad07285ddb41f57f59f676fb65b15568481
Krzysztof Bijakowski <krzysztof.bijakowski@cloudify.co>
2018-07-05 11:30:50 +0200

    Execute conditionally workflow added

--- resource_management_plugin/tasks.py
+++ resource_management_plugin/tasks.py
@@ -34,3 +34,37 @@ def check_resources_availability(ctx,
         'Validation of resource profile started ...'
     )
     engine.validate_profile(project_id, profile)
+
+
+@workflow
+def execute_conditionally(ctx,
+                          execution_dict,
+                          project_id,
+                          profile_name=None,
+                          profile_str=None,
+                          mode=DEFAULT_MODE,
+                          **kwargs):
+
+    rest_client = manager.get_rest_client()
+    engine = Engine(ctx, rest_client)
+    profile = get_profile(rest_client, profile_name, profile_str)
+
+    engine.run(MODES[mode])
+    ctx.logger.info(
+        'Calculating resources availability finished.\n'
+        'Validation of resource profile started ...'
+    )
+
+    engine.validate_profile(project_id, profile)
+    ctx.logger.info(
+        'Resource profile validated successfully - starting execution {}'
+        .format(execution_dict)
+    )
+
+    execution = rest_client.executions.start(**execution_dict)
+    ctx.logger.info(
+        'Execution {} started successfully !'.format(execution)
+    )
+
+
+
